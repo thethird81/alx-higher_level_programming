97dfca72d757a840318d997647d96a3b6b9d9711
thethird81 <thethird81@gmail.com>
2022-09-26 02:40:52 +0000

    again

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,8 +3,4 @@
 
 def print_list_integer(my_list=[]):
     for num in my_list:
-<<<<<<< HEAD
         print("{:d}".format(num))
-=======
-        print("{:d}".format(num))
->>>>>>> 5fec55e52a88832627d7b4fab06ae1f766bcc0d3
